c71c0a7cf38b26dd9190ac9bd9e700d31f746adb
faifai1411 <fai25491411@gmail.com>
2025-06-18 11:41:25 +0000

    ex01

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,7 +1,4 @@
-def main():
-    original = [2,8, 9, 48, 8, 22, -12, 2]
-    new = [x + 2 for x in original]
-
-    print("Original array:",original)
-    print("New array:",new)
-main()
+numbers =  [2, 8, 9, 48, 8, 22, -12, 2]
+plus_two = [data + 2 for data in numbers]
+print(f'Original array: {numbers}')
+print(f'New array: {plus_two}')
